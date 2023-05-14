47994e367f556fd82e7f533c9aac7a20cc87cd11
Hepheir <hepheir@gmail.com>
2023-05-14 23:24:54 +0900

    feat: create reply.models.Reply

--- [과제 #3] 게시판 모델 만들기/mysite/reply/models.py
+++ [과제 #3] 게시판 모델 만들기/mysite/reply/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from ..comment.models import Comment
+from ..user.models import User
 
 # Create your models here.
+
+
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment.id, null=False, on_delete=models.CASCADE)
+    author = models.ForeignKey(User.id, null=False, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
