a54ef3b4a01be89b6b551db090b71fb234d2f674
Hepheir <hepheir@gmail.com>
2023-05-14 23:24:40 +0900

    feat: create comment.models.Comment

--- [과제 #3] 게시판 모델 만들기/mysite/comment/models.py
+++ [과제 #3] 게시판 모델 만들기/mysite/comment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from ..post.models import Post
+from ..user.models import User
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post.id, null=False, on_delete=models.CASCADE)
+    author = models.ForeignKey(User.id, null=False, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
