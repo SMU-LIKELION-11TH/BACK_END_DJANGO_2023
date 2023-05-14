fa558293674b939edb842ad129384c05c168d761
seoyeseon <dptjs6476@gmail.com>
2023-05-15 01:45:07 +0900

    서예선 - 5주차 과제
    아직 많이 완성되지 않은 과제입니다..

---
+++ 5주차 과제/mysite/comment/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Comment(models.Model):
+     liked = models.IntegerField()
+     detail = models.CharField(max_length=50)
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now_add=True)
+     post = models.ForeignKey('post.Post', on_delete=models.CASCADE)
+     user = models.ForeignKey('user.User', on_delete=models.CASCADE)
---
+++ 5주차 과제/mysite/reply/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Reply(models.Model):
+    liked = models.IntegerField()
+    detail = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    comment = models.ForeignKey('comment.Comment', on_delete=models.CASCADE)
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE)
