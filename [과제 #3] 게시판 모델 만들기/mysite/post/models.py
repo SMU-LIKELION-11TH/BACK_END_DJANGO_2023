6a97e8e6ae29dd92b82b6313bb897974624e1bc0
Hepheir <hepheir@gmail.com>
2023-05-14 23:24:32 +0900

    feat: create post.models.Post

--- [과제 #3] 게시판 모델 만들기/mysite/post/models.py
+++ [과제 #3] 게시판 모델 만들기/mysite/post/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from ..user.models import User
 
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User.id, null=False, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
