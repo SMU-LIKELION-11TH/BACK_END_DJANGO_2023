2be2005d28c094b53fa144069da9db65d94d9f6a
seoyeseon <dptjs6476@gmail.com>
2023-05-15 14:42:40 +0900

    서예선 - 5주차 과제 수정

--- 5주차 과제 수정필요/mysite/comment/models.py
+++ 5주차 과제 수정필요/mysite/comment/models.py
@@ -5,6 +5,6 @@ class Comment(models.Model):
      liked = models.IntegerField()
      detail = models.CharField(max_length=50)
      created_at = models.DateTimeField(auto_now_add=True)
-     updated_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
      post = models.ForeignKey('post.Post', on_delete=models.CASCADE)
      user = models.ForeignKey('user.User', on_delete=models.CASCADE)
--- 5주차 과제 수정필요/mysite/reply/models.py
+++ 5주차 과제 수정필요/mysite/reply/models.py
@@ -5,6 +5,6 @@ class Reply(models.Model):
     liked = models.IntegerField()
     detail = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     comment = models.ForeignKey('comment.Comment', on_delete=models.CASCADE)
     user = models.ForeignKey('user.User', on_delete=models.CASCADE)
