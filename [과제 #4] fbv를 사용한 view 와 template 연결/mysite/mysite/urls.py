c47a05395317263175865cad34dff9396778f4eb
Hepheir <hepheir@gmail.com>
2023-05-24 00:04:38 +0900

    feat(과제 04): add posts/ to urls.py

--- [과제 #4] fbv를 사용한 view 와 template 연결/mysite/mysite/urls.py
+++ [과제 #4] fbv를 사용한 view 와 template 연결/mysite/mysite/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import post.views as post
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("posts/", post.render_sample_template),
 ]
