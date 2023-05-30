75f6fff64d517b2c9df62fd549b8ac3dc5f29cc6
CastleBravo01 <96935944+CastleBravo01@users.noreply.github.com>
2023-05-31 02:28:35 +0900

    model.py 수정
    아직 Orders클래스와 Orders_Menus클래스 간 관계가 불안정합니다. 고칠점 부탁드립니다.

--- 과제#3/mysite/post/models.py
+++ 과제#3/mysite/post/models.py
@@ -1,30 +1,33 @@
-import datetime
 from django.db import models
 
-class User(models.Model):
-    user = models.CharField(max_length=20)
-    password = models.CharField(max_length=50)
+class Users(AbstractUser):
     address = models.CharField(max_length=256)
 
 class Orders(models.Model):
-    orderer = models.CharField(max_length=20)
-    ordered_food = models.CharField(max_length=100)
-    number_of_food_ordered = models.IntegerField()
-    ordered_from = models.CharField(max_length=256)
+    orderer_id = models.ForeignKey(Users.username, on_delete=models.CASCADE)
+    shipping_address = models.ForeignKey(Users.address, on_delete=models.CASCADE)
     additional_request = models.CharField(max_length=256)
     updated_at = models.DateTimeField(auto_now=True)
 
-class store(models.Model):
+#models.ManyToManyField(Store, through='Order_Store', through_fields=('ordered_menu',))
+class Stores(models.Model):
     store_name = models.CharField(max_length=20)
-    address = models.CharField(max_length=256)
+    store_address = models.CharField(max_length=256)
+    def __str__(self):
+        return self.store_name
 
-class menu(models.Model):
+class Menus(models.Model):
+    store_name = models.ForeignKey(Stores.store_name, on_delete=models.CASCADE)
     menu_name = models.CharField(max_length=20)
-    price = models.CharField(max_length=256)
+    menu_price = models.CharField(max_length=256)
 
-class rating(models.Model):
-    store_name = models.CharField(max_length=20)
-    username = models.CharField(max_length=20)
+class Ratings(models.Model):
+    store_name = models.ForeignKey(Stores.store_name, on_delete=models.CASCADE)
+    username = models.ForeignKey(Users.username, on_delete=models.CASCADE)
     star_rating = models.IntegerField()
     comment = models.CharField(max_length=256)
 
+class Orders_Menus(models.Model):
+    orderer = models.ForeignKey(Users.username, on_delete=models.CASCADE)
+    ordered_menu = models.ForeignKey(Menus.menu_name, on_delete=models.CASCADE)
+    number_of_orders = models.IntegerField()
--- 과제#3/mysite/post/views.py
+++ 과제#3/mysite/post/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def test(request):
     name = 'username'
