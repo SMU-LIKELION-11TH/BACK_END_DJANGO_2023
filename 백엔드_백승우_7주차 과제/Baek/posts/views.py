d3232012e462311efaabe20a970dd4756342d8eb
BaekSeungW00 <ph1losophe@naver.com>
2023-06-01 22:27:08 +0900

    백승우-7주차 과제

--- 백엔드_백승우_7주차 과제/Baek/posts/views.py
+++ 백엔드_백승우_7주차 과제/Baek/posts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse, reverse_lazy
 
 from .models import Post
 from users.models import User
 from comments.models import Comment
+
 
 class PostDetailView(DetailView):
     model = Post
@@ -70,35 +71,35 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
         queryset = queryset.filter(user=self.request.user)
         return queryset
 
-class CommentCreateView(LoginRequiredMixin, CreateView):
-    model = Comment
-    template_name = 'posts_detail.html'
-    fields = ['content']
-    pk_url_kwarg = 'post_id'
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        form.instance.post_id = self.kwargs['post_id']
-        return super().form_valid(form)
+class CommentCreateView(LoginRequiredMixin, View):
+    def get(self, post_id):
+        return redirect('post_detail', post_id=post_id)
 
-    def get_success_url(self,):
-        post_id = self.object.post.id
-        return reverse_lazy('post_detail', args=[self.get_object().post.id])
+    def post(self, request, post_id):
+        content = request.POST.get('comment_content')
+        user = request.user
+        post = Post.objects.get(id=post_id)
+        comment = Comment.objects.create(content=content, user=user, post=post)
+        comment.save()
+        return redirect('post_detail', post_id=post_id)
 
-class CommentUpdateView(LoginRequiredMixin, UpdateView):
-    model = Comment
-    template_name = 'posts_detail.html'
-    fields = ['content']
-    pk_url_kwarg = 'comment_id'
+class CommentUpdateView(LoginRequiredMixin, View):
+    def get(self, comment_id):
+        comment = Comment.objects.get(id=comment_id)
+        return redirect('post_detail', post_id=comment.post.id)
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        queryset = queryset.filter(user=self.request.user)
-        return queryset
+    def post(self, request, comment_id):
+        comment = Comment.objects.get(id=comment_id)
+        if request.user == comment.user:
 
-    def get_success_url(self):
-        return reverse_lazy('post_detail', args=[self.get_object().post.id])
+            content = request.POST.get('comment_new_content')
 
+            comment.content = content
+            comment.save()
+
+            return redirect('post_detail', post_id=comment.post.id)
+        return redirect('post_detail', post_id=comment.post.id)
 class CommentDeleteView(LoginRequiredMixin, DeleteView):
     model = Comment
     template_name = 'posts_detail.html'
